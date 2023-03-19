e68931c8f13a13a8456fe9f0912b54aaa8a672fb
ArtKolyach <artyom.kolyach@yandex.ru>
2023-03-19 22:59:56 +0300

    feat(time change algorithms): add multiple time changing and formatting functions, add comments

--- main-script.js
+++ main-script.js
@@ -1,29 +1,87 @@
+// Convert to 24h format
+const convertTo24HourFormat = (hours, AmPm) => {
+    switch (AmPm) {
+        case 'PM':
+            if (hours !== 12) hours += 12;
+            break;
+        case 'AM':
+            if (hours === 12) hours = 0;
+            break;
+    }
+    return hours;
+}
+
+
+// Convert String time to Date
+const convertToDate = (timeText) => {
+    const timeTextSplit = timeText.split(' ');
+    const AmPm = timeTextSplit[1];
+
+    const timeHoursAndMinutes = timeTextSplit[0].split(':');
+    let hours = Number(timeHoursAndMinutes[0]);
+    const minutes = Number(timeHoursAndMinutes[1]);
+
+    hours = convertTo24HourFormat(hours, AmPm);
+
+    return new Date(new Date().setHours(hours, minutes, 0, 0));
+}
+
+
+// Get timezone offset in minutes
 const ET_TO_UTC_DIFF_IN_MIN = 300;
 const LocalToUTC = new Date().getTimezoneOffset();
 const ETtoLocal = ET_TO_UTC_DIFF_IN_MIN - LocalToUTC;
 console.log(ETtoLocal);
 
-const convertETToLocal = (date) => { //КОНВЕРТАЦИЯ Date В ЛОКАЛЬНОЕ ВРЕМЯ Date
+
+// Convert Date to local time Date
+const convertETToLocal = (date) => {
     const convertedDate = new Date(date);
     convertedDate.setMinutes(date.getMinutes() + ETtoLocal);
     return new Date(convertedDate);
 }
 
-const formatTime = (oldTimeStamp) => {
 
-    let hour = Number(oldTimeStamp[0]);
-    hour -= 4;
-
-    let newTimeStamp
-    if (hour < 0){
-        hour = 24 + hour;
-        newTimeStamp = hour + oldTimeStamp.slice(1, 5) + 'MSK';
-    }
-    else newTimeStamp = hour + oldTimeStamp.slice(1, 5) + 'MSK+1';
-
-    return newTimeStamp;
+// Get timezone name if there is one
+const getTimezoneName = () => {
+    const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    const timezoneSplit = timezone.split('/');
+    if (timezoneSplit[1]) return timezoneSplit[1];
+    else if (timezoneSplit[0]) return timezoneSplit[0];
+    else return '';
 }
 
+
+// Convert Date back to String time
+const convertDateToTimeText = (date) => {
+    const splitDate = date.toTimeString().split(':');
+    const hoursAndMinutes = splitDate[0] + ':' + splitDate[1];
+
+    let timeText = hoursAndMinutes + ' ' + getTimezoneName();
+
+    if (dayHasChanged) timeText = '*' + timeText;
+    return timeText.toUpperCase();
+}
+
+let dayHasChanged = false; // Day has changed by timezone offset or not variable
+
+
+// MAIN FORMATTING FUNCTION
+const formatTime = (oldTimeStamp) => {
+    const oldDate = convertToDate(oldTimeStamp);
+
+    const localOldDate = convertETToLocal(oldDate);
+
+    dayHasChanged = localOldDate.getDate() !== oldDate.getDate();
+
+    console.log(localOldDate.getDate(), oldDate.getDate());
+
+    return convertDateToTimeText(localOldDate);
+
+}
+
+
+// Get query selector for current page
 const url = window.location.href;
 const SCHEDULE_URL = 'https://www.mlb.com/schedule';
 const MAIN_PAGE_URL = 'https://www.mlb.com/';
@@ -36,6 +94,8 @@ const getQuerySelector = (url) => {
     }
 }
 
+
+// GET TIME FIELDS AND CHANGE THEM
 const changeTime = () => {
     const querySelector = getQuerySelector(url);
 
@@ -51,4 +111,5 @@ const changeTime = () => {
     console.log('НАСЕРЕНО В formatAllTimes', url, querySelector);
 }
 
+// EXECUTION
 window.onload = changeTime;
